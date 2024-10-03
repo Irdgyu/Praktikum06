67c337edc782b826d984533d0d4977bbe96ad569
Irdgyu <gilangbayuirwana@gmail.com>
2024-10-03 12:48:54 +0700

    Tahun kabisat apa bukan?

--- Pemilihan2Percobaan106.java
+++ Pemilihan2Percobaan106.java
@@ -12,7 +12,10 @@ public class Pemilihan2Percobaan106 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
-            }       
+            }    
+            else {
+                System.out.println("Bukan Termasuk Tahun Kabisat ");
+            }   
         }
         else {
             System.out.println("Bukan Tahun Kabisat");
