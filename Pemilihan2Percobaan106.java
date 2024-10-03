0e68fdfbb41a04b2fcbec5814838ca976cdaef02
Irdgyu <gilangbayuirwana@gmail.com>
2024-10-03 13:17:54 +0700

    Tahun 2000

--- Pemilihan2Percobaan106.java
+++ Pemilihan2Percobaan106.java
@@ -12,10 +12,15 @@ public class Pemilihan2Percobaan106 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
-            }    
-            else {
-                System.out.println("Bukan Termasuk Tahun Kabisat ");
             }   
+            else if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println("Tahun kabisat");
+                }
+                else {
+                    System.out.println("Bukan tahun kabisat");
+                }
+            }
         }
         else {
             System.out.println("Bukan Tahun Kabisat");
