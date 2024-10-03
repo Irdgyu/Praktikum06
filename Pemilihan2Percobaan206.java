5320cd894007fa789067b44e748e64b75ddf8776
Irdgyu <gilangbayuirwana@gmail.com>
2024-10-03 15:10:40 +0700

    Cafe JTI

---
+++ Pemilihan2Percobaan206.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan206 {
+
+    public static void main(String[] args) {
+        Scanner input6 = new Scanner(System.in);
+
+        String member;
+        int jumlahBeli, menu;
+        double diskon, harga, totalBayar;
+
+        System.out.println("-----------------------");
+        System.out.println("==== MENU CAFE JTI ====");
+        System.out.println("-----------------------");
+        System.out.println("1. KOPI");
+        System.out.println("2. TEH");
+        System.out.println("3. ROTI");
+        System.out.println("-----------------------------------------");
+        System.out.println("Masukkan angka dari menu yang dipilih = ");
+        menu = input6.nextInt();
+        input6.nextLine();
+        System.out.println("Apakah anda punya member? (ya/tidak) = ");
+        member = input6.nextLine();
+        System.out.println("-----------------------------------------");
+
+        if (member.equalsIgnoreCase("ya")) {
+            diskon = 0.10;
+            System.out.println("Besar diskon = 10% ");
+            if (menu == 1) {
+                harga = 12000;
+                System.out.println("Harga kopi = " + harga);
+
+            } else if (menu == 2) {
+                harga = 7000;
+                System.out.println("Harga teh = " + harga);
+
+            } else if (menu == 3) {
+                harga = 20000;
+                System.out.println("Harga roti = " + harga);
+                
+            } else {
+                System.out.println("Masukkan menu dengan benar");
+                return;
+            }
+
+            totalBayar = harga - (harga * diskon);
+            System.out.println("Total pembayaran setelah diskon = " + totalBayar);
+        }
+        
+        else if (member.equalsIgnoreCase("tidak")) {
+            if (menu == 1) {
+                harga = 12000;
+                System.out.println("Harga kopi = " + harga);
+
+            } else if (menu == 2) {
+                harga = 7000;
+                System.out.println("Harga teh = " + harga);
+
+            } else if (menu == 3) {
+                harga = 20000;
+                System.out.println("Harga roti = " + harga);
+                
+            } else {
+                System.out.println("Masukkan menu dengan benar");
+                return;
+            }
+            
+            System.out.println("Total pembayaran = " + harga);
+        } else {
+            System.out.println("Mmeber tidak valid");
+        }
+        System.out.println("-----------------------------------------");  
+    }
+}
